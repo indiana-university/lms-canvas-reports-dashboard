092da15b85995c7e9b34067e0f74c941c080d6cd
Damian Sobieralski <dsobiera@iu.edu>
2026-09-21 14:22:38 -0400

    LMSA-9886 - initial upgrade

--- src/test/java/edu/iu/uits/lms/reports/services/ReportListingRepositoryTest.java
+++ src/test/java/edu/iu/uits/lms/reports/services/ReportListingRepositoryTest.java
@@ -40,7 +40,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
